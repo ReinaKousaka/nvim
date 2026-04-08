03cd349adb9f5b7620412720e7030bf11b796154
Songheng Yin <yinsongheng@gmail.com>
2026-04-08 14:51:35 +0900

    update nordic colorscheme, better visual lines

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,8 +4,13 @@ return {
         lazy = false,
         priority = 1000,
         config = function()
+            require("nordic").setup({
+                bold_keywords = true,
+                bright_border = true,
+                visual = { theme = "light", blend = 0.95 },
+                cursorline = { theme = "light", blend = 0.5 },
+            })
             require("nordic").load()
         end,
     },
 }
-
